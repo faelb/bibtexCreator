5730f4b05cd0681db54f31910fc50b546ecb6b17
faelb <waelbslife@gmail.com>
2022-01-29 15:18:45 +0100

    [+] added program code

---
+++ creator.py
@@ -0,0 +1,20 @@
+def main():
+    refIndex = input("Enter refIndex")
+    author = input("Enter author")
+    journal = input("Enter journal")
+    title = input("Enter title")
+    year = input("Enter year")
+    url = input("Enter url")
+    accessed = "29.01.2022" #constant - set it yourself
+
+
+    blueprint = '@Article{'+refIndex+',\r\n  author  = {'+author+'},\r\n  journal = {'+journal+'},' \
+                '\r\n  title   = {'+title+'},\r\n  year    = {'+year+'},\r\n  note    = {' \
+                +accessed+'},\r\n  url     = {' \
+                +url+'},\r\n} '
+
+    print(blueprint)
+
+
+if __name__ == "__main__":
+    main()
