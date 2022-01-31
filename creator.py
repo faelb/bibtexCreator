5d0df0e150d237e6ed92e24733c110e81432ac8e
Christoph Felber <waelbslife@gmail.com>
2022-01-31 21:04:27 +0100

    [*]bugfix_added exceptions if there is no datetime object

--- creator.py
+++ creator.py
@@ -1,26 +1,31 @@
+from datetime import date
+
 from newsplease import NewsPlease
 import pyperclip3
 import base64
 import hashlib
 
+
 def crawlData(url):
     article = NewsPlease.from_url(url)
     return article
+
+
 def main():
     url = input("Enter url")
-    #url="https://www.smartinsights.com/social-media-marketing/social-media-strategy/new-global-social-media-research/"
+    # url="https://www.smartinsights.com/social-media-marketing/social-media-strategy/new-global-social-media-research/"
     article = crawlData(url)
-    #create a refIndex with a hash and use first 10 digits, to be sure its unique
+    # create a refIndex with a hash and use first 10 digits, to be sure its unique
     hash = hashlib.sha1(url.encode("utf-8"))
     refIndex = base64.urlsafe_b64encode(hash.digest()[:5]).decode("utf-8")
 
-    #parse journal name out of URL
+    # parse journal name out of URL
     if "www." in article.source_domain:
         journal = article.source_domain[4:]
     else:
         journal = article.source_domain
 
-    #get authors or else journal is author
+    # get authors or else journal is author
     author = ""
     if len(article.authors) == 0:
         author = journal
@@ -33,9 +38,15 @@ def main():
             author = author[:-5]
 
     title = article.title
-    year = article.date_publish.strftime("%Y")
-    accessed = article.date_download.strftime("%d") +"." + article.date_download.strftime("%m") +"." +  article.date_download.strftime("%Y")
-
+    try:
+        year = article.date_publish.strftime("%Y")
+    except:
+        year = "2022"
+    try:
+        accessed = article.date_download.strftime("%d") + "." + article.date_download.strftime(
+            "%m") + "." + article.date_download.strftime("%Y")
+    except:
+        accessed = date.today().strftime("%d") + "." + date.today().strftime("%m") + "." + date.today().strftime("%Y")
 
     print("please confirm or change the parameters:")
     print(f"author = {author}")
@@ -66,10 +77,10 @@ def main():
     print("This is your Bibtex:")
     print("")
     print("")
-    blueprint = '@Article{'+refIndex+',\r\n  author  = {'+author+'},\r\n  journal = {'+journal+'},' \
-                '\r\n  title   = {'+title+'},\r\n  year    = {'+year+'},\r\n  note    = {' \
-                +"accessed: "+accessed+'},\r\n  url     = {' \
-                +url+'},\r\n} '
+    blueprint = '@Article{' + refIndex + ',\r\n  author  = {' + author + '},\r\n  journal = {' + journal + '},' \
+                                                                                                           '\r\n  title   = {' + title + '},\r\n  year    = {' + year + '},\r\n  note    = {' \
+                + "accessed: " + accessed + '},\r\n  url     = {' \
+                + url + '},\r\n} '
 
     print(blueprint)
     print("")
@@ -77,6 +88,7 @@ def main():
     print("-------------------------------------------------")
     print("The bibtext was stored to your clipboard (use Ctrl V to paste it)")
     pyperclip3.copy(blueprint)
+
 
 if __name__ == "__main__":
     main()
